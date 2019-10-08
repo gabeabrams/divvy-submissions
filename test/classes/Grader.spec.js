e0519865751e08f845e5d9c2f0031d63e680b469
Shihan Li <lshhenry98@gmail.com>
2019-10-08 14:17:47 -0400

    added test for grader class

--- test/classes/Grader.spec.js
+++ test/classes/Grader.spec.js
@@ -3,14 +3,30 @@ const Grader = require('../../classes/Grader');
 
 describe('classes > Grader', function () {
   it('returns correct values for getter, setter function', async function () {
-    const bob = new Grader();
     const fakeSubmissionsArr = [
       {
-        
+        id: 2,
+        studentIds: [3, 4, 5],
+        isSubmitted: true,
       },
       {
-
+        id: 5,
+        studentIds: [9, 8, 7],
+        isSubmitted: false,
       },
     ];
+
+    const fakeGraderId = 12456;
+
+    const bob = new Grader(fakeGraderId, fakeSubmissionsArr);
+    assert(bob.getNumToGrade() === -1, 'did not initialize numToGrade to -1');
+    assert(
+      bob.getAllowedSubmissions() === fakeSubmissionsArr,
+      'getter function for submissions did not return correct value'
+    );
+    assert(
+      bob.getId() === fakeGraderId,
+      'getter function for graderid did not return correct value'
+    );
   });
 });
