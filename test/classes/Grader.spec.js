def8ac6f3db214eed870fd23e6a84d3e09bcf09f
Shihan Li <lshhenry98@gmail.com>
2019-10-08 12:22:26 -0400

    added grader test shell

---
+++ test/classes/Grader.spec.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const Grader = require('../../classes/Grader');
+
+describe('classes > Grader', function () {
+  it('returns correct values for getter, setter function', async function () {
+    const bob = new Grader();
+    const fakeSubmissionsArr = [
+      {
+        
+      },
+      {
+
+      },
+    ];
+  });
+});
