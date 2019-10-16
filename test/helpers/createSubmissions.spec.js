76123fd43bc7142dd99d8ee39125e391ae1dc5aa
Shihan Li <lshhenry98@gmail.com>
2019-10-16 16:38:42 -0400

    added test for group homeworks

--- test/helpers/createSubmissions.spec.js
+++ test/helpers/createSubmissions.spec.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const createSubmissions = require('../../helpers/createSubmissions');
 
 describe('helpers > createSubmissions', function () {
-  it.only('returns correct submissions for individual homeworks', async function () {
+  it('returns correct submissions for individual homeworks', async function () {
     // create fake data
     const fakeStudents = [
       {
@@ -56,6 +56,78 @@ describe('helpers > createSubmissions', function () {
   });
 
   it('return correct submissions in group assignments', async function () {
+    // create fake data
+    const fakeStudents = [
+      {
+        id: 1,
+        isSubmitted: true,
+      },
+      {
+        id: 2,
+        isSubmitted: true,
+      },
+      {
+        id: 3,
+        isSubmitted: false,
+      },
+      {
+        id: 4,
+        isSubmitted: true,
+      },
+      {
+        id: 5,
+        isSubmitted: false,
+      },
+      {
+        id: 6,
+        isSubmitted: false,
+      },
+    ];
 
+    const fakeGroup = [
+      [1, 2, 3],
+      [4],
+      [5, 6],
+    ];
+
+    const opts = {
+      students: fakeStudents,
+      groups: fakeGroup,
+    };
+
+    const res = createSubmissions(opts);
+    assert.equal(res.length, 3, 'did not return correct number of submissions');
+
+    // spot check return submission object to make sure values are correct
+    assert.equal(
+      JSON.stringify(res[0].getStudentIds()),
+      JSON.stringify([1, 2, 3]),
+      'did not create correct studentIds accroding to students passed in'
+    );
+    assert.equal(
+      res[0].getIsSubmitted(),
+      true,
+      'did not create correct isSubmitted according to students passed in'
+    );
+    assert.equal(
+      JSON.stringify(res[1].getStudentIds()),
+      JSON.stringify([4]),
+      'did not create correct studentIds accroding to students passed in'
+    );
+    assert.equal(
+      res[1].getIsSubmitted(),
+      true,
+      'did not create correct isSubmitted according to students passed in'
+    );
+    assert.equal(
+      JSON.stringify(res[2].getStudentIds()),
+      JSON.stringify([5, 6]),
+      'did not create correct studentIds accroding to students passed in'
+    );
+    assert.equal(
+      res[2].getIsSubmitted(),
+      false,
+      'did not create correct isSubmitted according to students passed in'
+    );
   });
 });
