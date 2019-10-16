22f4bd49668bc64339ec642038a2414566d5247e
Shihan Li <lshhenry98@gmail.com>
2019-10-16 13:54:21 -0400

    Added shell for create submissions test?

---
+++ test/helpers/createSubmissions.spec.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const Submission = require('../../classes/Submission');
+const createSubmissions = require('../../helpers/createSubmissions');
+
+describe('helpers > createSubmissions', function () {
+  it('returns correct submissions for individual homeworks', async function () {
+
+  });
+
+  it('return correct submissions in group assignments', async function () {
+
+  });
+});
