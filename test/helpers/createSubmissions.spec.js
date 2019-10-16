f8b4097f257ff3a576d4e55d9629ec48619759d4
Shihan Li <lshhenry98@gmail.com>
2019-10-16 16:21:53 -0400

    added test for create submissions with individual homeworks

--- test/helpers/createSubmissions.spec.js
+++ test/helpers/createSubmissions.spec.js
@@ -1,10 +1,58 @@
 const assert = require('assert');
-const Submission = require('../../classes/Submission');
 const createSubmissions = require('../../helpers/createSubmissions');
 
 describe('helpers > createSubmissions', function () {
-  it('returns correct submissions for individual homeworks', async function () {
+  it.only('returns correct submissions for individual homeworks', async function () {
+    // create fake data
+    const fakeStudents = [
+      {
+        id: 1,
+        isSubmitted: true,
+      },
+      {
+        id: 2,
+        isSubmitted: true,
+      },
+      {
+        id: 3,
+        isSubmitted: false,
+      },
+      {
+        id: 4,
+        isSubmitted: true,
+      },
+    ];
 
+    const fakeGroup = [];
+
+    const opts = {
+      students: fakeStudents,
+      groups: fakeGroup,
+    };
+
+    // spot check return submission object to make sure values are correct
+    createSubmissions(opts).forEach((returnedSubmission, i) => {
+      // check if studentIds are correct
+      assert.equal(
+        JSON.stringify(returnedSubmission.getStudentIds()),
+        JSON.stringify([i + 1]),
+        'did not create correct studentIds accroding to students passed in'
+      );
+      // check if isSubmitted is correct
+      if (i === 2) {
+        assert.equal(
+          returnedSubmission.getIsSubmitted(),
+          false,
+          'did not create correct isSubmitted according to students passed in'
+        );
+      } else {
+        assert.equal(
+          returnedSubmission.getIsSubmitted(),
+          true,
+          'did not create correct isSubmitted according to students passed in'
+        );
+      }
+    });
   });
 
   it('return correct submissions in group assignments', async function () {
