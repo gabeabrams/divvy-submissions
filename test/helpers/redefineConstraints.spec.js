f1efed9ce696b1babed19a30de1db66a22b0e89c
Shihan Li <lshhenry98@gmail.com>
2019-10-21 11:49:52 -0400

    added testing for banned pairing

--- test/helpers/redefineConstraints.spec.js
+++ test/helpers/redefineConstraints.spec.js
@@ -219,4 +219,125 @@ describe('helpers > redefineConstraints', function () {
       );
     });
   });
+
+  it('returns right list if student banned by all graders', async function () {
+    // create fake data
+    const fakeSubmissions = [
+      new Submission([1, 2], true),
+      new Submission([3], true),
+      new Submission([4, 5, 6], false),
+    ];
+
+    const fakeGraders = [
+      {
+        id: 1,
+        proportionalWorkload: 1,
+      },
+      {
+        id: 2,
+        proportionalWorkload: 1,
+      },
+      {
+        id: 3,
+        proportionalWorkload: 1,
+      },
+    ];
+
+    const fakeBannedPairs = [
+      {
+        grader: 1,
+        student: 3,
+      },
+      {
+        grader: 2,
+        student: 3,
+      },
+      {
+        grader: 3,
+        student: 3,
+      },
+    ];
+
+    const fakeRequiredPairs = [];
+
+    const opts = {
+      submissions: fakeSubmissions,
+      graders: fakeGraders,
+      bannedPairs: fakeBannedPairs,
+      requiredPairs: fakeRequiredPairs,
+    };
+
+    // based on our fake data, the expected output is listed below
+    const expectedGraders = [
+      {
+        id: 1,
+        allowedSubmissions: [
+          {
+            id: 1,
+            studentIds: [1, 2],
+            isSubmitted: true,
+          },
+          {
+            id: 3,
+            studentIds: [4, 5, 6],
+            isSubmitted: true,
+          },
+        ],
+        proportionalWorkload: 1,
+        numToGrade: -1,
+      },
+      {
+        id: 2,
+        allowedSubmissions: [
+          {
+            id: 1,
+            studentIds: [1, 2],
+            isSubmitted: true,
+          },
+          {
+            id: 3,
+            studentIds: [4, 5, 6],
+            isSubmitted: true,
+          },
+        ],
+        proportionalWorkload: 1,
+        numToGrade: -1,
+      },
+      {
+        id: 3,
+        allowedSubmissions: [
+          {
+            id: 1,
+            studentIds: [1, 2],
+            isSubmitted: true,
+          },
+          {
+            id: 3,
+            studentIds: [4, 5, 6],
+            isSubmitted: true,
+          },
+        ],
+        proportionalWorkload: 1,
+        numToGrade: -1,
+      },
+    ];
+
+    const graders = redifineConstraints(opts);
+
+    // spot checking each post processed fields
+    graders.forEach((grader, i) => {
+      // verify allowed submissions is correct
+      assert.equal(
+        graders[i].allowedSubmissions.studentIds,
+        expectedGraders[i].allowedSubmissions.studentIds,
+        'did not return correct allowed submission studentIDs'
+      );
+      // verify isSubmitted is correct
+      assert.equal(
+        graders[i].allowedSubmissions.isSubmitted,
+        expectedGraders[i].allowedSubmissions.isSubmitted,
+        'did not return correct isSubmitted'
+      );
+    });
+  });
 });
