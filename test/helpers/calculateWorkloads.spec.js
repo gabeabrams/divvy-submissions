21fbafb912f1bbe76831e20c0b94a5a77ebfef55
Shihan Li <lshhenry98@gmail.com>
2019-10-21 21:44:51 -0400

    added test for first distribution

--- test/helpers/calculateWorkloads.spec.js
+++ test/helpers/calculateWorkloads.spec.js
@@ -3,17 +3,23 @@ const Grader = require('../../classes/Grader');
 const calculateWorkloads = require('../../helpers/calculateWorkloads');
 
 describe('helpers > calculateWorkloads', function () {
-  it('returns correct workloads for each grader', async function () {
-    // just check the sum of the graders equal to total submissions, and they don't differ by too much
+  it.only('returns correct workloads when divides evenly', async function () {
     // create fake data
     const fakeGraders = [
-      new Grader(1, [], 2),
-      new Grader(2, [], 2),
+      new Grader(1, [], 1),
+      new Grader(2, [], 1),
       new Grader(3, [], 1),
     ];
 
-    const fakeNumSubmissions = 8;
+    const fakeNumSubmissions = 6;
 
-    calculateWorkloads(fakeGraders, fakeNumSubmissions);
+    const res = calculateWorkloads(fakeGraders, fakeNumSubmissions);
+    res.forEach((grader) => {
+      assert.equal(
+        grader.getNumToGrade(),
+        2,
+        'did not assign correct number of submission to grade'
+      );
+    });
   });
 });
