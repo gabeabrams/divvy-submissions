e6282212ede1109730c3269b0d7a691416d09c79
Shihan Li <lshhenry98@gmail.com>
2019-10-21 22:04:01 -0400

    added testing for dividing unevenly case

--- test/helpers/calculateWorkloads.spec.js
+++ test/helpers/calculateWorkloads.spec.js
@@ -3,7 +3,7 @@ const Grader = require('../../classes/Grader');
 const calculateWorkloads = require('../../helpers/calculateWorkloads');
 
 describe('helpers > calculateWorkloads', function () {
-  it.only('returns correct workloads when divides evenly', async function () {
+  it('returns correct workloads when divides evenly', async function () {
     // create fake data
     const fakeGraders = [
       new Grader(1, [], 1),
@@ -13,7 +13,10 @@ describe('helpers > calculateWorkloads', function () {
 
     const fakeNumSubmissions = 6;
 
+    // call function
     const res = calculateWorkloads(fakeGraders, fakeNumSubmissions);
+
+    // spot check if each grader gets assigned equal number of submissions
     res.forEach((grader) => {
       assert.equal(
         grader.getNumToGrade(),
@@ -21,5 +24,54 @@ describe('helpers > calculateWorkloads', function () {
         'did not assign correct number of submission to grade'
       );
     });
+  });
+
+  it('returns correct workloads when divides evenly 2', async function () {
+    // create fake data
+    const fakeGraders = [
+      new Grader(1, [], 1),
+      new Grader(2, [], 2),
+      new Grader(3, [], 3),
+    ];
+
+    const fakeNumSubmissions = 6;
+
+    // call function
+    const res = calculateWorkloads(fakeGraders, fakeNumSubmissions);
+
+    // spot check if each grader gets assigned equal number of submissions
+    res.forEach((grader, i) => {
+      assert.equal(
+        grader.getNumToGrade(),
+        i + 1,
+        'did not assign correct number of submission to grade'
+      );
+    });
+  });
+
+  it('returns correct workloads if does not divide evenly', async function () {
+    // create fake data
+    const fakeGraders = [
+      new Grader(1, [], 1),
+      new Grader(2, [], 2),
+      new Grader(3, [], 2),
+    ];
+
+    const fakeNumSubmissions = 6;
+
+    // call function
+    const res = calculateWorkloads(fakeGraders, fakeNumSubmissions);
+
+    let subAssigned = 0;
+    res.forEach((grader) => {
+      subAssigned += grader.getNumToGrade();
+    });
+
+    // check if all submissions have been assigned
+    assert.equal(
+      subAssigned,
+      fakeNumSubmissions,
+      'did not assign every submission to grader'
+    );
   });
 });
