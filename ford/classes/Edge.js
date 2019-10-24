6ac59c304089cf5106e2a82d6c49a1a388ab329a
Shihan Li <lshhenry98@gmail.com>
2019-10-24 13:50:08 -0400

    added edge class

--- ford/classes/Edge.js
+++ ford/classes/Edge.js
@@ -1,6 +1,75 @@
-// TODO: implement
-// - start node
-// - end node
-// - weight (float)
-// - capacity (float)
-// - flow (float)
+class Edge {
+  /**
+   * Create a new Edge instance
+   * @param {Node} startNode - the start node of this edge
+   * @param {Node} endNode - the end node of this edge
+   * @param {number} weight - the weight of this edge
+   * @param {number} capacity - the maximum flow that can go through this edge
+   * @param {number} flow - the current flow passing through this edge
+   */
+  constructor(opts) {
+    const {
+      startNode,
+      endNode,
+      weight,
+      capacity,
+      flow,
+    } = opts;
+
+    this.startNode = startNode;
+    this.endNode = endNode;
+    this.weight = weight;
+    this.capacity = capacity;
+    this.flow = flow;
+  }
+
+  /**
+   * Get start node
+   * @return {Node} the start node of this edge
+   */
+  getStartNode() {
+    return this.startNode;
+  }
+
+  /**
+   * Get end node
+   * @return {Node} the end node of this edge
+   */
+  getendNode() {
+    return this.endNode;
+  }
+
+  /**
+   * Get edge weight
+   * @return {number} the weight of this edge
+   */
+  getWeight() {
+    return this.weight;
+  }
+
+  /**
+   * Set edge weight
+   * @param {number} - new weight for this edge
+   */
+  setWeight(newWeight) {
+    this.weight = newWeight;
+  }
+
+  /**
+   * Get capacity
+   * @return {number} the capacity of this edge
+   */
+  getCapacity() {
+    return this.capacity;
+  }
+
+  /**
+   * Get flow
+   * @return {number} the flow of this edge
+   */
+  getFlow() {
+    return this.flow;
+  }
+}
+
+module.exports = Edge;
