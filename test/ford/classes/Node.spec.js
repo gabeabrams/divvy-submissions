4d6c9ae56ca6e8507e63c84ab7756af424bb6e31
Shihan Li <lshhenry98@gmail.com>
2019-10-24 14:46:38 -0400

    added test for node class

---
+++ test/ford/classes/Node.spec.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const Node = require('../../../ford/classes/Node');
+
+describe('classes > Node', function () {
+  it('returns correct values for getter, setter function', async function () {
+    // Create fake data
+    const type = 'sink';
+    const metadata = {
+      graderId: '132',
+    };
+
+    // Create new node instanace
+    const node = new Node(type, metadata);
+
+    // Test getters
+    assert.equal(
+      node.getNodeId(),
+      1,
+      'did not generate correct id for node'
+    );
+
+    assert.equal(
+      node.getNodeType(),
+      type,
+      'getter for type not working'
+    );
+
+    assert.equal(
+      JSON.stringify(node.getOutgoingEdges()),
+      JSON.stringify([]),
+      'getter for outgoing edges not working'
+    );
+
+    assert.equal(
+      node.getMetadata(),
+      metadata,
+      'getter for metadata not working'
+    );
+
+
+    // Test setters
+    const newOutgoingEdges = [3, 4, 5];
+    node.setOutgoingEdges(newOutgoingEdges);
+
+    assert.equal(
+      JSON.stringify(node.getOutgoingEdges()),
+      JSON.stringify(newOutgoingEdges),
+      'setter for outgoing edges not working correctly'
+    );
+  });
+});
