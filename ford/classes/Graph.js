920d11b9ccc1756f313aa881a1aeead74af7550a
Shihan Li <lshhenry98@gmail.com>
2019-10-28 15:32:08 -0400

    added shell for graph class

--- ford/classes/Graph.js
+++ ford/classes/Graph.js
@@ -2,3 +2,17 @@
 // - findPath (function) that runs a shortest path algorithm [NEXT WEEK]
 // - _updateGraph (path) that updates the flow on the graph based on the path
 //     that was found [NEXT WEEK]
+
+class Graph {
+  /**
+   * Create a new Node instance
+   * @param {Submission[]} submissions - a list of submissions
+   * @param {Graders[]} graders - a list of graders
+   */
+  constructor(submissions, graders) {
+    this.submissions = submissions;
+    this.graders = graders;
+  }
+}
+
+module.exports = Graph;
