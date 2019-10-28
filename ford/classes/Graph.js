286a4ba927ec403d682a19a2b5035511969b36f4
Shihan Li <lshhenry98@gmail.com>
2019-10-28 17:14:23 -0400

    added test shell for graph class, did not compile

--- ford/classes/Graph.js
+++ ford/classes/Graph.js
@@ -2,7 +2,6 @@ const Node = require('./Node');
 const Edge = require('./Edge');
 
 class Graph {
-  // - source (node) the source node for this graph
   // - findPath (function) that runs a shortest path algorithm [NEXT WEEK]
   // - _updateGraph (path) that updates the flow on the graph based on the path
   //     that was found [NEXT WEEK]
