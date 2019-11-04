40ae4e6147e625187c4ed7ccdf19ae4d23738963
Shihan Li <lshhenry98@gmail.com>
2019-11-04 09:50:01 -0500

    fixed bug

--- ford/classes/Graph.js
+++ ford/classes/Graph.js
@@ -71,7 +71,7 @@ class Graph {
           startNode: graderNode,
           endNode: submissionNode,
           weight: (
-            submissionIsAllowed
+            isAllowed
               ? 1
               : this.NOT_ALLOWED_WEIGHT
               // ^ Add high-weight edge because this pairing is not allowed
