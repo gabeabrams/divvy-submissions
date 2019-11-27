0a919c773244307767048a31e853df74ec006870
Shihan Li <lshhenry98@gmail.com>
2019-11-27 16:54:16 -0500

    added test for when a grader is required to grade more than their workload

--- test/index.spec.js
+++ test/index.spec.js
@@ -271,6 +271,7 @@ describe('index', function () {
       isDeterministic: true,
     };
 
+    // running the algorithm
     const { constraintViolations } = main(opts);
 
     const expectedDescription = 'This grader is banned from grading this submission.';
@@ -449,15 +450,262 @@ describe('index', function () {
       5: 2,
     };
 
-    // running this test multiple times can result in different pairings,
-    // showing randomizaiton works
+    // run the algorithm
     const { studentToGraderMap } = main(opts);
+
+    // check if studentToGraderMap matches expected result
     Object.keys(studentToGraderMap).forEach((studentId) => {
       assert.equal(
         studentToGraderMap[studentId],
         expectedStudentToGrader[studentId],
         'did not return correct pairing in a group assignment'
       );
+    });
+  });
+
+  it('returns correct pairing in complex problem', async function () {
+    // the full list of student entries in the form: { id, isSubmitted }
+    const students = [
+      {
+        id: 1,
+        isSubmitted: true,
+      },
+      {
+        id: 2,
+        isSubmitted: true,
+      },
+      {
+        id: 3,
+        isSubmitted: true,
+      },
+      {
+        id: 4,
+        isSubmitted: true,
+      },
+      {
+        id: 5,
+        isSubmitted: true,
+      },
+      {
+        id: 6,
+        isSubmitted: true,
+      },
+    ];
+
+    // the full list of grader entries in the form: { id, proportionalWorkload }
+    const graders = [
+      {
+        id: 1,
+        proportionalWorkload: 1,
+      }, {
+        id: 2,
+        proportionalWorkload: 2,
+      },
+    ];
+
+    // a list of pairs in the form:
+    // { grader: <grader id>, student: <student id> }
+    const bannedPairs = [
+      {
+        grader: 1,
+        student: 2,
+      },
+      {
+        grader: 1,
+        student: 3,
+      },
+      {
+        grader: 1,
+        student: 4,
+      },
+    ];
+
+    // a list of pairs in the form:
+    // { grader: <grader id>, student: <student id> }
+    const requiredPairs = [
+      {
+        grader: 1,
+        student: 1,
+      },
+      {
+        grader: 2,
+        student: 2,
+      },
+      {
+        grader: 2,
+        student: 6,
+      },
+    ];
+
+    const opts = {
+      students,
+      graders,
+      bannedPairs,
+      requiredPairs,
+      isDeterministic: true,
+    };
+
+    const expectedStudentToGrader = {
+      1: 1,
+      2: 2,
+      3: 2,
+      4: 2,
+      5: 1,
+      6: 2,
+    };
+
+    const expectedWorkloadMap = {
+      1: 2,
+      2: 4,
+    };
+
+    // run the algorithm
+    const {
+      studentToGraderMap,
+      workloadMap,
+      constraintViolations,
+    } = main(opts);
+
+    // check if studentToGraderMap matches expected result
+    Object.keys(studentToGraderMap).forEach((studentId) => {
+      assert.equal(
+        studentToGraderMap[studentId],
+        expectedStudentToGrader[studentId],
+        'did not return correct pairing in a group assignment'
+      );
+    });
+
+    // check if workloadMap matches expected result
+    Object.keys(workloadMap).forEach((graderId) => {
+      assert.equal(
+        workloadMap[graderId],
+        expectedWorkloadMap[graderId],
+        'did not return correct workload'
+      );
+    });
+
+    // check if there are no constraint violations
+    assert.equal(
+      constraintViolations.length,
+      0,
+      'did not return correct violations'
+    );
+  });
+
+  it.only('returns correct pairing when required > workload', async function () {
+    // the full list of student entries in the form: { id, isSubmitted }
+    const students = [
+      {
+        id: 1,
+        isSubmitted: true,
+      },
+      {
+        id: 2,
+        isSubmitted: true,
+      },
+      {
+        id: 3,
+        isSubmitted: true,
+      },
+      {
+        id: 4,
+        isSubmitted: true,
+      },
+    ];
+
+    // the full list of grader entries in the form: { id, proportionalWorkload }
+    const graders = [
+      {
+        id: 1,
+        proportionalWorkload: 1,
+      }, {
+        id: 2,
+        proportionalWorkload: 1,
+      },
+    ];
+
+    // a list of pairs in the form:
+    // { grader: <grader id>, student: <student id> }
+    const bannedPairs = [];
+
+    // a list of pairs in the form:
+    // { grader: <grader id>, student: <student id> }
+    const requiredPairs = [
+      {
+        grader: 1,
+        student: 1,
+      },
+      {
+        grader: 1,
+        student: 2,
+      },
+      {
+        grader: 1,
+        student: 3,
+      },
+    ];
+
+    const opts = {
+      students,
+      graders,
+      bannedPairs,
+      requiredPairs,
+      isDeterministic: true,
+    };
+
+    const expectedStudentToGrader = {
+      1: 1,
+      2: 1,
+      3: 2,
+      4: 2,
+    };
+
+    const expectedWorkloadMap = {
+      1: 2,
+      2: 2,
+    };
+
+    const expectedViolations = [{
+      englishDescription: 'A different grader is required to grade this submission.',
+      type: 'required',
+      listOfStudentsInvolved: [3],
+      listOfGradersInvolved: [1],
+    }];
+
+    // run the algorithm
+    const {
+      studentToGraderMap,
+      workloadMap,
+      constraintViolations,
+    } = main(opts);
+
+    // check if studentToGraderMap matches expected result
+    Object.keys(studentToGraderMap).forEach((studentId) => {
+      assert.equal(
+        studentToGraderMap[studentId],
+        expectedStudentToGrader[studentId],
+        'did not return correct pairing in a group assignment'
+      );
+    });
+
+    // check if workloadMap matches expected result
+    Object.keys(workloadMap).forEach((graderId) => {
+      assert.equal(
+        workloadMap[graderId],
+        expectedWorkloadMap[graderId],
+        'did not return correct workload'
+      );
+    });
+
+    // check if constraintViolations matches expected result
+    constraintViolations.forEach((violation, i) => {
+      Object.keys(violation).forEach((violationField) => {
+        assert.equal(
+          JSON.stringify(violation[violationField]),
+          JSON.stringify(expectedViolations[i][violationField]),
+          'did not return correct violations'
+        );
+      });
     });
   });
 });
